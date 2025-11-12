838f869f98324406a49f7cd9b091c7c932c7caeb
ChandrakanthChandru <chandruchandrakanth2004@gmail.com>
2025-11-12 07:51:52 +0530

    first changes

---
+++ Portfolio.js
@@ -0,0 +1,36 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function toggleMenu() {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+function closeMenu() {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+  var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "SQL Developer ", "Web Developer" ,"DataBase Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+// Scroll Reveal Animation for Contact Page
+const revealElements = document.querySelectorAll('.scroll-reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('revealed');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+revealElements.forEach(el => observer.observe(el));
+  
